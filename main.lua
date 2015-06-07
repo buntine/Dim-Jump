226fed65900fe4f75f4f741aa989713ef3c3ebaf
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-06-07 15:58:04 +1000

    Simple level rendering

--- main.lua
+++ main.lua
@@ -7,13 +7,20 @@ function love.load(a)
     y = (love.graphics.getHeight() - 100),
     w = 20,
     h = 20,
-    speed = 120
+    speed = 130,
+    level = 1
+  }
+
+  levels = {
+    { {100, 20, 20}, {360, 20, 20}, {600, 20, 20} },
+    { {160, 20, 20}, {360, 20, 20}, {500, 20, 20}, {700, 20, 20} }
   }
 end
 
 function love.update(dt)
   if (player.x + player.w) > love.graphics.getWidth() then
     player.x = 0
+    player.level = player.level + 1
   else
     player.x = player.x + (player.speed * dt)
   end
@@ -22,6 +29,7 @@ end
 function love.draw(dt)
   drawFloor()
   drawPlayer()
+  drawLevel()
 end
 
 function drawPlayer()
@@ -30,4 +38,12 @@ end
 
 function drawFloor()
   love.graphics.rectangle("fill", 0, (player.y + player.h), love.graphics.getWidth(), 80)
+end
+
+function drawLevel()
+  obstacles = levels[player.level]
+
+  for _, o in ipairs(obstacles) do
+    love.graphics.rectangle("fill", o[1], (player.y + player.h) - o[3], o[2], o[3])
+  end
 end
