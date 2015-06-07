c092abbfc093e00fa5e7f12e7fe27c8431d35399
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-06-07 15:29:24 +1000

    Floor based on players Y

--- main.lua
+++ main.lua
@@ -1,19 +1,28 @@
 function love.load(a)
   love.graphics.setBackgroundColor(255, 255, 255)
   love.graphics.setColor(80, 80, 80)
+
+  player = {
+    x = 0,
+    y = (love.graphics.getHeight() - 100),
+    w = 20,
+    h = 20,
+    speed = 80
+  }
 end
 
-function love.update(td)
+function love.update(dt)
+  player.x = player.x + (player.speed * dt)
 end
 
-function love.draw(td)
-  makeFloor()
+function love.draw(dt)
+  drawFloor()
+  drawPlayer()
 end
 
-function makeFloor()
+function drawPlayer()
 end
 
-function makeFloor()
-  love.graphics.rectangle("fill", 0, (love.graphics.getHeight() - 80),
-                          love.graphics.getWidth(), 80)
+function drawFloor()
+  love.graphics.rectangle("fill", 0, (player.y + player.h), love.graphics.getWidth(), 80)
 end
