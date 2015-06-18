f4bb5465e7306027698b21d91a52d86e1c509a24
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-06-18 23:24:31 +1000

    Added jump sfx

--- main.lua
+++ main.lua
@@ -7,10 +7,14 @@ function love.load(a)
   love.graphics.setColor(255, 255, 255, 255)
   love.graphics.setNewFont(18)
 
+  love.audio.setVolume(0.5)
+
   love.keyboard.setKeyRepeat(true)
 
   title = love.graphics.newImage("assets/title.png")
   dim_queue = love.graphics.newImage("assets/dim_queue.png")
+
+  jump_sfx = love.audio.newSource("assets/sounds/jump4.wav")
 
   world = World:new{ground=love.graphics.getHeight() - 80}
   player = Player:new{world=world}
@@ -71,6 +75,7 @@ end
 function love.keypressed(key, isrepeat)
   if key == "up" or key == " " then
     if player.alive and not player.jumping then
+      love.audio.play(jump_sfx)
       player:jump()
     elseif not player.alive then
       player = Player:new{world=world}
